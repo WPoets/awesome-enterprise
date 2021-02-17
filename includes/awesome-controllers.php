976b2f21140bc8e3d45a5a7e3033e373c18791d0
Amit <thecancerus@gmail.com>
2021-02-17 09:00:25 +0530

    commented the code till we take a call on how to handle core layout.

--- includes/awesome-controllers.php
+++ includes/awesome-controllers.php
@@ -870,9 +870,9 @@ class controllers{
 
 		}
 		
-		if(!isset($app['active']['collection']['post_type']) && aw2_library::post_exists('layout',AWESOME_CORE_POST_TYPE)){
+		/* if(!isset($app['active']['collection']['post_type']) && aw2_library::post_exists('layout',AWESOME_CORE_POST_TYPE)){
 				return aw2_library::module_run(['post_type'=>AWESOME_CORE_POST_TYPE],'layout',null,null);
-		}	
+		}	 */
 		
 		// well none of the layout options exists so hand it over to page.php
 		 
