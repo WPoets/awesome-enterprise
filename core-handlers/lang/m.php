a89b5f9dd48791874167dc140889cc19a4702a1a
Amit <thecancerus@gmail.com>
2021-10-14 17:18:08 +0530

    fixed: Parse error: syntax error, unexpected 'empty' (T_EMPTY), expecting ')'

--- core-handlers/lang/m.php
+++ core-handlers/lang/m.php
@@ -498,7 +498,7 @@ function num_round($value, $atts){
 
 
 //return empty array
-\aw2_library::add_service(‘m.empty_array’,‘given empty array in return. Use m.empty_array’,[‘namespace’=>__NAMESPACE__]);
+\aw2_library::add_service('m.empty_array','given empty array in return. Use m.empty_array',['namespace'=>__NAMESPACE__]);
 function empty_array($value, $atts){
 	$value = array();
 	return $value;
