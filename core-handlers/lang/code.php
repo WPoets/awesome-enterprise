1f0d43bc00d6d5f2db9bd69f97352019016ec401
Amit Singh <amit@wpoets.com>
2022-08-16 08:17:06 +0530

    Update code.php
    added support for code.dump - it will allows us the out the awesome code without parsing, useful when we are writing help.

--- core-handlers/lang/code.php
+++ core-handlers/lang/code.php
@@ -5,7 +5,7 @@ namespace aw2\code;
 
 
 \aw2_library::add_service('code.run','Run the Code Library',['namespace'=>__NAMESPACE__]);
-function run($atts,$content=null,$shortcode){
+function run($atts,$content=null,$shortcode=null){
 	if(\aw2_library::pre_actions('all',$atts,$content)==false)return;
 	extract(\aw2_library::shortcode_atts( array(
 	'main'=>null
@@ -14,5 +14,14 @@ function run($atts,$content=null,$shortcode){
 	$return_value=\aw2_library::get($main,$atts,$content);
 	$return_value=\aw2_library::parse_shortcode($return_value);	
 	$return_value=\aw2_library::post_actions('all',$return_value,$atts);
+	return $return_value;
+}
+
+
+\aw2_library::add_service('code.dump','Run the Code Library',['namespace'=>__NAMESPACE__]);
+function dump($atts,$content=null,$shortcode=null){
+	if(\aw2_library::pre_actions('all',$atts,$content)==false)return;
+	
+	$return_value='<code>' . $content . '</code>';	
 	return $return_value;
 }
