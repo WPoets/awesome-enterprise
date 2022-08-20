a2d2209c2d7e8bb615a6593f0ea797f18d821d28
Amit Singh <amit@wpoets.com>
2022-08-20 20:25:07 +0530

    added support for js.run_on_activity
    added support for js.run_on_activity to manage 3rd party scripts.

--- core-handlers/front-end/js.php
+++ core-handlers/front-end/js.php
@@ -28,3 +28,84 @@ function minify($atts,$content=null,$shortcode){
 	$return_value=\aw2_library::post_actions('all',$return_value,$atts);
 	return $return_value;	
 }
+
+\aw2_library::add_service('js.run_on_activity','Load scripts and runs the JS code on user interation',['namespace'=>__NAMESPACE__]);
+
+function run_on_activity($atts,$content=null,$shortcode){
+	if(\aw2_library::pre_actions('all',$atts,$content)==false)return;
+	
+	extract(\aw2_library::shortcode_atts( array(
+	'src'=>null
+	), $atts) );
+	
+	if(empty($src) && empty($content)) return;
+
+	$chars='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' ;
+	$uniqueid=\substr( \str_shuffle( $chars ), 0, 12 );
+	
+	$string='
+	<script>
+	var app_'.$uniqueid.' = {
+        init: function() {
+            window.addEventListener("scroll", function() {
+                if (window.__'.$uniqueid.' == undefined) {
+                    app_'.$uniqueid.'.load();
+                }
+            });
+            window.addEventListener("mousemove", function() {
+                if (window.__'.$uniqueid.' == undefined) {
+                    app_'.$uniqueid.'.load();
+                }
+            });
+             window.addEventListener("keydown", function() {
+                if (window.__'.$uniqueid.' == undefined) {
+                    app_'.$uniqueid.'.load();
+                }
+            });
+             window.addEventListener("touchstart", function() {
+                if (window.__'.$uniqueid.' == undefined) {
+                    app_'.$uniqueid.'.load();
+                }
+            });
+        },';
+    
+    if(!empty($src)){
+		  $string .=' load: function() {
+            var script = document.createElement("script");
+            script.src = "'.$src.'";
+            document.head.appendChild(script);
+            script.onload = function() {
+            ';
+            if(!empty($content)){
+				$string .=$content;
+			}    
+			
+           $string .=' window["__he"] = true;
+            }
+            window["__'.$uniqueid.'"] = true;
+        }';
+	} else {
+		  $string .=' load: function() {
+            ';
+            if(!empty($content)){
+				$string .=$content;
+			}    
+			
+           $string .=' 
+            window["__'.$uniqueid.'"] = true;
+        }';
+	}    
+  
+    
+        
+    $string .=' };
+    
+    app_'.$uniqueid.'.init(); </script>';
+	
+	$minifier = new \MatthiasMullie\Minify\JS();
+	$minifier->add($string);
+	
+	$return_value = $minifier->minify();
+	$return_value=\aw2_library::post_actions('all',$return_value,$atts);
+	return $return_value;	
+}
