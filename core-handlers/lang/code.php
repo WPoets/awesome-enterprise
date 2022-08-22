6b576d89bd3793a5d29453d1212b580d08e3940c
Amit Singh <amit@wpoets.com>
2022-08-22 23:00:20 +0530

    added support for code highlighter
    added code.highlight to show syntax highlight using prism.js. #todo - support syntax of aw3

--- core-handlers/lang/code.php
+++ core-handlers/lang/code.php
@@ -18,10 +18,32 @@ function run($atts,$content=null,$shortcode=null){
 }
 
 
-\aw2_library::add_service('code.dump','Run the Code Library',['namespace'=>__NAMESPACE__]);
+\aw2_library::add_service('code.dump','Dump the Code as is without parsing',['namespace'=>__NAMESPACE__]);
 function dump($atts,$content=null,$shortcode=null){
 	if(\aw2_library::pre_actions('all',$atts,$content)==false)return;
 	
-	$return_value='<code>' . $content . '</code>';	
+	$return_value='<code>' . trim($content) . '</code>';	
+	return $return_value;
+}
+
+\aw2_library::add_service('code.highlight','Dump the code with syntax highlighter',['namespace'=>__NAMESPACE__]);
+function highlight($atts,$content=null,$shortcode=null){
+	if(\aw2_library::pre_actions('all',$atts,$content)==false)return;
+	
+	extract(\aw2_library::shortcode_atts( array(
+	'lang'=>'html'
+	), $atts) );
+	$return_value='';
+	
+	\aw2_library::set('@prismjs.active','yes');
+	\aw2_library::set('@prismjs.active','no');
+	if(\aw2_library::get('@prismjs.active')==='no'){
+	
+		$return_value .='<script type="spa/axn" axn="core.run_script" cdn_js_files="prismjs/prism.js" cdn_css_files="prismjs/prism.css"></script>';
+	}
+	
+	$return_value .='<pre class="line-numbers"><code class="language-'.$lang.'">'.$content .'</code></pre>';
+	
+		
 	return $return_value;
 }
