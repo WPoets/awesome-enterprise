a334fcb405eede7c094fa46a7be263e6991a5ebf
Amit Singh <amit@wpoets.com>
2023-02-21 17:08:05 +0530

    Update redis_db.php
    added new shortcode _redis_db.get_ to ensure that we can read any ticket code across any redis db.

--- core-handlers/cache/redis_db.php
+++ core-handlers/cache/redis_db.php
@@ -127,3 +127,45 @@ function stream_fetch_usage($atts,$content=null,$shortcode){
 	return $return_value;	
 }
 
+
+
+\aw2_library::add_service('redis_db.get','Get values of a ticket',['namespace'=>__NAMESPACE__]);
+
+function get($atts,$content=null,$shortcode=null){
+	
+	if(\aw2_library::pre_actions('all',$atts,$content,$shortcode)==false)return;
+	
+	extract(\aw2_library::shortcode_atts( array(
+	'main'=>null,
+	'field'=>null,
+	'db'=>null
+	), $atts) );
+
+	if(!$main)return 'Main must be set';		
+	
+	$ticket=$main;
+	if(is_null($db)){
+		return '';
+		
+	}
+	$redis = \aw2_library::redis_connect($db);
+
+	$return_value='';	
+		
+	if($field)
+		$return_value=$redis->hGet($ticket,$field);
+	else	
+		$return_value=$redis->hGetAll($ticket); 
+			
+	if($field==='ticket_expiry'){
+		$ttl=$redis->ttl($ticket);
+		$return_value=round($ttl/60,2);
+	}		
+	
+
+
+
+	$return_value=\aw2_library::post_actions('all',$return_value,$atts);
+	return $return_value;
+	
+}
